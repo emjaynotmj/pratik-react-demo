43b7800cf9612a8fe74307162e0a851c10c3155d
Emjay <abdulmujeeb.jamiu@andela.com>
2018-04-27 02:28:31 +0100

    use promise.all to handle multiple update asynchronously

--- server/server.js
+++ server/server.js
@@ -1,28 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://emjay:12345678@ds229648.mlab.com:29648/bucketlist');
-
 const app = express();
 
+mongoose.connect('mongodb://localhost/albumnic');
+const BucketListSchema = new mongoose.Schema({
+  name: String,
+  description: String
+});
+const BucketListModel = mongoose.model('buckets', BucketListSchema);
+
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-const BucketListSchema = new mongoose.Schema({
-  name: String,
-  description: String
-});
-
-const BucketListModel = mongoose.model('buckets', BucketListSchema);
 
 app.post('/buckets', (req, res) => {
-  BucketListModel.create({name: req.body.name, description: req.body.description}).then(bucket => {
+  BucketListModel.create({ name: req.body.name, description: req.body.description }).then(bucket => {
     res.send(bucket);
   }).catch(err => res.send(err));
 });
@@ -30,6 +29,21 @@ app.post('/buckets', (req, res) => {
 app.get('/buckets', (req, res) => {
   BucketListModel.find().then(buckets => {
     res.send(buckets);
+  }).catch(err => res.send(err))
+});
+
+app.put('/update-many', (req, res) => {
+  BucketListModel.find().then(buckets => {
+    const arr_of_bucket_promises = buckets.map((bucket, i) => {
+      const newBucket = bucket;
+      newBucket.name = `An activity ${i + 1}`;
+      return BucketListModel.findByIdAndUpdate(bucket._id, newBucket)
+    });
+    return Promise.all(arr_of_bucket_promises).then(allRes => {
+      res.send(allRes);
+    }).catch(err => {
+      res.send(err);
+    })
   }).catch(err => res.send(err))
 });
 
